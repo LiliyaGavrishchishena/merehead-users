ade37f8eab323b2a2f050423f82115bf0b3953ff
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2020-02-12 15:00:47 +0200

    concurrently added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+//styles
 import './index.css';
+// components
 import App from './components/App';
+// store
 import store from './store/store';
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
